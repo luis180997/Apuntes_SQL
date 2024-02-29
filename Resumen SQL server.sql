9fa53aaf907b2fd2299fe4bf865563768c9b91a3
Luis Agüero <78891730+luis180997@users.noreply.github.com>
2024-02-29 17:20:14 -0500

    Add files via upload

--- Resumen SQL server.sql
+++ Resumen SQL server.sql
@@ -103,14 +103,12 @@ SELECT TOP 2 * FROM tabla_importada;
 -- Seleccion del 50% de los datos
 SELECT TOP 50 PERCENT * FROM tabla_importada;
 
-
 CREATE TABLE tabla_prueba(
 	id varchar(20) not null, -- al utilizar NOT NULL el campo no puede estar vacio
 	nombre varchar(50) null, -- no es necesario poner NULLL para que el campo pueda estar vacio
 	edad int,
 	sueldo decimal(19,2)
 );
-
 
 -- CONSTRAINTS - PRIMARY KEY
 CREATE TABLE tabla_prueba(
@@ -200,6 +198,13 @@ CREATE TABLE ordenes(
 	id_cliente int
 	constraint FK_ordenes_clientes foreign key references clientes(id_cliente)
 );
+-- otra forma (la mejor)
+CREATE TABLE ordenes(
+	id_orden int not null,
+	articulo varchar(50) not null,
+	id_cliente int
+	constraint FK_ordenes_clientes foreign key (id_cliente) references clientes(id_cliente) -- el segundo id_cliente es de la tabla cliente
+);
 
 DROP TABLE clientes;
 
@@ -208,96 +213,284 @@ DROP TABLE clientes;
 Comentarios para mas de una linea
 */
 
+-- Vistas
+CREATE VIEW clientes_nacionales
+AS
+SELECT * FROM clientes WHERE edad < 25;
 
-CREATE TABLE Empleados (
-    ID INT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    Departamento VARCHAR(50),
-    Salario DECIMAL(10, 2)
+-- Indices
+CREATE CLUSTERED INDEX I_idempleado --nombre del indice
+ON tabla_empleados(columna_id);
+CREATE NONCLUSTERED INDEX I_edad_empleado --nombre del indice
+ON tabla_empleados(columna_edad);
+-- Cambia el nombre del indice
+EXEC sp_rename 'tabla_empleado.I_idempleado', 'I_id', 'INDEX';
+-- Eliminar indice
+DROP INDEX I_id ON tabla_empleado;
+
+-- DISTINCT 
+SELECT DISTINCT pais FROM clientes;
+
+-- ALIAS Y CONCATENACION
+SELECT nombre + ' ' + apellido + CAST(edad AS VARCHAR(2)) nombre_apellido_edad FROM empleados;
+
+-- ESQUEMAS
+CREATE SCHEMA ventas;
+CREATE TABLE ventas.cliente(
+ idclientes int
 );
 
-INSERT INTO Empleados VALUES (1,'luis', 'TI', 1500);
-INSERT INTO Empleados VALUES (2,'manuel', 'TI', 1800);
-INSERT INTO Empleados VALUES (3,'alfredo', 'comercial', 2500);
-INSERT INTO Empleados VALUES (4,'juan', 'ventas', 1400);
-INSERT INTO Empleados VALUES (5,'andrea', 'ventas', 1000);
-INSERT INTO Empleados VALUES (6,'jose', 'TI', 2000);
+-- ORDER BY
+SELECT * FROM Empleados ORDER BY Nombre DESC;
 
-SELECT * FROM Empleados;
+-- FUNCIONES DE AGRUPACION: COUNT, SUM, AVG
+SELECT COUNT(*) FROM Empleados; -- Hace un conteo de registros
+SELECT COUNT(DepartamentoID) FROM Empleados;
 
-SELECT Departamento, COUNT(*) AS Cantidad 
-FROM Empleados
-GROUP BY Departamento
-ORDER BY Cantidad  DESC;
+-- OPERADORES: AND, OR Y NOT 
+SELECT * FROM clientes
+WHERE pais = 'Italia' AND ciudad IN ('Roma','Venecia') AND NOT pais = 'Canada';
 
-SELECT Nombre, Salario
-FROM Empleados
-WHERE Salario > (SELECT AVG(Salario) FROM Empleados);
+-- BETWEEN
+SELECT * FROM empleados WHERE id_empleado IN(1,3,5,7);
+SELECT * FROM empleados WHERE id_empleado BETWEEN 1 AND 7 OR cantidad BETWEEN 1 AND 3;
+SELECT * FROM empleados WHERE pais BETWEEN 'Alemania' AND 'Mexico';
+SELECT * FROM empleados WHERE fecha BETWEEN '1999-06-01' AND '2005-06-01';
 
-SELECT Nombre, Salario
-FROM Empleados
-GROUP BY Nombre
-HAVING Salario > AVG(Salario);
+-- OPERADORES: LIKE Y NOT LIKE
+SELECT * FROM clientes
+WHERE nombre LIKE 'A%'; -- Nombres que comiencen con a
+SELECT * FROM clientes
+WHERE nombre LIKE '%A'; -- Nombres que acaben en a 
+SELECT * FROM clientes
+WHERE nombre LIKE '%A%'; -- Nombres que tengan una a
+SELECT * FROM clientes
+WHERE nombre LIKE '__A%'; -- ignora el primer y segundo caracter
+SELECT * FROM clientes
+WHERE nombre LIKE 'a%o'; -- Nombre que comienza con a y termina en o
+SELECT * FROM clientes
+WHERE nombre NOT LIKE '%A'; -- Nombres que no terminan en A
+
+-- INNER JOIN, LEFT JOIN, RIGHT JOIN, FULL JOIN
+-- https://youtu.be/a4sMtXZjmHk?si=O4Lw0_hkq8Vqg_US
+
+-- CLAUSULA UNION, se debe terner el mismo numero de columnas
+SELECT columna1, columna2 FROM tabla1
+UNION
+SELECT columna1, columna2 FROM tabla2;
+-- muestra los datos dublicados UNION ALL
+SELECT columna1, columna2 FROM tabla1
+UNION ALL
+SELECT columna1, columna2 FROM tabla2;
+
+-- GROUP BY
+SELECT AVG(idcliente) AS promedio, pais
+FROM clientes
+GROUP BY pais ORDER BY promedio DESC;
+
+-- HAVING
+SELECT producto, SUM(cantidad) AS cantidad_total, AVG(precio_unitario) AS precio_promedio
+FROM ventas
+WHERE precio_unitario > 10
+GROUP BY producto
+HAVING SUM(cantidad) > 1000;
+
+-- SUBCONSULTAS
+-- Reglas:
+-- * Las subconsuiltas deben ir entre parentesis
+-- * No pueden contener IN, ANY, ALL y EXISTS
+-- * No pueden contener BETWEEN ni LIKE ni ORDER BY
+-- Ejemplo 1: Cuales empleados ganan mas o igual al salario promedio
+SELECT id_empleado, nombre, apellido, sueldo
+FROM empleados WHERE sueldo >= (SELECT AVG(sueldo) FROM empleados);
+-- Ejemplo 2:  Buscar todos los nombre de clientes con idcliente de Mexico
+SELECT nombre, ciudad FROM clientes
+WHERE idcliente = ANY(SELECT idcliente FROM clientes
+						WHERE pais = 'Mexico');
+-- Ejemplo 3: Cuales clientes han comprado lapices
+SELECT cliente, numero, fecha FROM facturas f
+WHERE NOT EXISTS (SELECT * FROM detalles d
+					WHERE f.numero = d.numerofactura
+					AND d.articulo = 'Lapiz');
+
+-- ISNULL, COALESCE
+-- reemplaza los valores NULL por '0'
+SELECT nombre, precio_unidad, ISNULL(vendidos, 0) FROM productos;
+SELECT nombre, precio_unidad, COALESCE(vendidos, 0) FROM productos;
+
+-- CASE
+-- Ejemplo 1
+SELECT nombre, cantidad,
+CASE
+	WHEN cantidad > 30 THEN 'Articulo con sobre existencia'
+	WHEN cantidad < 10 THEN 'Se debe realizar pedido'
+	ELSE 'Existencia normal'
+END AS Inventario
+FROM articulos;
+-- Ejemplo 2
+SELECT nombre, ciudad, pais FROM clientes
+ORDER BY
+(CASE
+	WHEN ciudad IS NULL THEN pais
+	ELSE ciudad
+END);
+
+-- FUNCIONES MATEMATICAS
+SELECT CEILING($321.19); -- funcion para maximo entero (funcion con datos de moneda)
+SELECT FLOOR(321.12); -- funcion para eliminar los decimales
+SELECT ROUND(123.1245, 4); -- funcion para rendondear
+SELECT POWER(4,2); -- funcion para potencia (a^b)
+SELECT RAND(); -- funcion para valores aleatorios
+SELECT SIN(PI()/2), TAN(PI()/4); -- funciones trignonometricas
+SELECT SQRT(81); -- funcion para raiz cuadrada
+
+-- FUNCIONES STRING
+SELECT nombre + CHAR(65);
+SELECT CONCAT('Hola ', nombre,' bienvenido');
+SELECT LEN('SQL server'); -- Funcion para saber el numero de caracteres incluyendo espacios
+SELECT LOWER('SQL server'); -- Convierte todo a minisculas
+SELECT UPPER('SQL server'); -- Convierte todo a mayusculas
+SELECT LEFT('hola a todos',1); -- Trae la cantidad de letras de izquierda
+SELECT RIGHT('hola a todos',1); -- Trae la cantidad de letras de derecha
+SELECT STUFF('hola a todos', 3, 5, 'amigos'); -- Reemplaza un rango de caracteres determinados
+-- Convierte la primera letra en mayuscula
+SELECT CONCAT(UPPER(LEFT('hola a todos',1)), LOWER(RIGHT('hola a todos', RIGHT('hola a todos')-1)));
+SELECT ltrim('  hola mundo'); -- Elimina espacios de lado izquierdo
+SELECT rtrim('hola mundo   '); -- Elimina espacios de lado derecho
+SELECT trim('  hola mundo  '); -- Elimina espacios de lado izquierdo y derecho
+SELECT REPLACE('hola mundo', 'h', 'H'); -- Reemplaza caracteres en los registros
+SELECT TRANSLATE('abcdef', 'ab', '00'); -- Reemplaza mas de un caracter
+SELECT TRANSLATE('[123.4,72.3]','[,]','( )') AS punto;
+SELECT REPLICATE('hola',6); -- Reeplica n veces los caracteres
+SELECT REVERSE('SQLserver'); -- Devuelve la cadena de caracteres al reves
+SELECT REVERSE(654321);
+
+-- FUNCIONES DATE  
+SELECT getdate() AS fecha; -- Se obtiene la fecha actual
+SELECT dateadd(DAY, 2, getdate()); -- Agrega una cantidad de dias
+SELECT dateadd(MONTH, 2, getdate()); -- Agrega una cantidad de meses
+SELECT dateadd(YEAR, -2, getdate()); -- Agrega una cantidad de años
+SELECT dateadd(HOUR, -2, getdate()); -- Agrega una cantidad de horas
+SELECT dateadd(MINUTE, -2, getdate()); -- Agrega una cantidad de dias
+
+SELECT * FROM facturas
+WHERE fecha BETWEEN '2018-01-01' AND dateadd(YEAR, 1, '2018-01-01')
+
+SELECT DATENAME(MONTH, GETDATE()) AS MES; -- ver el nombre del mes
+SET LANGUAGE SPANISH; -- Esablecer idioma para la fecha
+SELECT DATEPART(MONTH, GETDATE()) AS MES; -- Ver mes en numero
+SELECT DATENAME(DAY, GETDATE()) AS MES;
+SELECT DATENAME(WEEKDAY, GETDATE()) AS MES; -- ver el nombre del dia
+
+-- TRANSACT-SQL, ROW_NUMBER(), FIRST_VALUE()
+SELECT * FROM (SELECT row_number() over(ORDER BY a.columna1 DESC) AS contador,
+a.id, a.columna1 FROM ventas AS a) AS a
+WHERE a.contador = 1;
+
+SELECT * FROM ventas a
+WHERE a.idvendedor = (
+					SELECT DISTINCT FIRST_VALUE(a.idvendedor) 
+					over(ORDER BY a.columna1 DESC) AS contador
+					FROM ventas a);
+
+-- CONTROL DE FLUJO
+DECLARE
+	--Variable1
+	--Variable2
+	--Variable3
+BEGIN
+--Codigo principal
+	if else
+	return
+	waitfor
+	while
+	for
+	do while
+	case
+-- control de eventualidades
+	continue
+	break
+END;
+
+-- T-SQL, IF EXIST() - ELSE
+IF EXISTS (SELECT * FROM articulos WHERE cantidad = 0)
+	(SELECT nombre, precio, cantidad
+	FROM articulos WHERE cantidad = 0)
+ELSE
+	SELECT 'No hay articulos en 0' AS resultado;
+
+SELECT * FROM sys.objects WHERE name = 'prueba';
+
+IF OBJECT_ID('prueba') IS NULL
+	DROP TABLE prueba;
+
+-- T-SQL VARIABLES
+DECLARE
+	@id_valor int,
+	@nombre VARCHAR(20),
+	@telefono numeric(10),
+	@fecha_nac date,
+	@activo bit;
+BEGIN
+	SET @id_valor = 40;
+	SET @nombre = 'Luis';
+	SET @telefono = 123456789;
+	SET @fecha_nac = '1980/10/01';
+	SET @activo = 'false';
+	SELECT @id_valor, @nombre;
+END;
+
+DECLARE
+	@patron VARCHAR(20);
+BEGIN
+	SET @patron = '%Lap%';
+	SELECT * FROM articulos WHERE nombre LIKE @patron;
+END;
+
+DECLARE
+	@mayorprecio DECIMAL(6,2);
+BEGIN
+	SELECT @mayorprecio = MAX(precio) FROM articulos;
+	SELECT * FROM articulos WHERE precio = @mayorprecio;
+END;
+
+-- VARIABLES TIPO TABLA
+-- Se tiene que ejecutar todo a la vez
+DECLARE
+	@tabla1 table(
+		id INT,
+		nombre VARCHAR(20),
+		telefono NUMERIC(20)
+	);
+	INSERT INTO @tabla1 VALUES (1,'Jose',123456);
+	SELECT * FROM @tabla1;
+
+-- PROCEDIMIENTOS ALMACENADOS (STORE PROCEDURE)
+-- Hay 4 prefijos:
+-- * sp_ (en master)
+-- * locales (del usuario)
+-- * temporales (#)
+-- * globales (##)
+CREATE PROC p_existencia
+AS
+SELECT * FROM articulos WHERE cantidad < 20;
+
+EXEC p_existencia; -- Ejecutar el procesimiento
+
+CREATE PROCEDURE p_bonificacion
+AS
+BEGIN 
+	IF EXISTS(SELECT * FROM empleados WHERE cantidad_hijos > 3)
+		BEGIN
+			UPDATE empleados SET sueldo = sueldo*0.20;
+			PRINT 'Se ha aplicado la bonificacion';
+		END
+	ELSE
+		BEGIN
+			PRINT 'No hay empleados con mas de 3 hijos';
+		END
+END;
+
+-- https://youtu.be/8sCrjt5e2Yk?si=waJrPohnD1fh5CxC
 
 
-CREATE TABLE Empleados (
-    ID INT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    DepartamentoID INT,
-    Salario DECIMAL(10, 2)
-);
-
-CREATE TABLE Departamentos (
-    ID INT PRIMARY KEY,
-    Nombre VARCHAR(50)
-);
-
-INSERT INTO Departamentos (ID, Nombre)
-VALUES
-(1, 'Ventas'),
-(2, 'TI'),
-(3, 'Recursos Humanos');
-
-INSERT INTO Empleados (ID, Nombre, DepartamentoID, Salario)
-VALUES
-(1, 'Juan Pérez', 1, 60000.00),
-(2, 'María González', 1, 55000.00),
-(3, 'Carlos Rodríguez', 2, 70000.00),
-(4, 'Ana López', 2, 75000.00),
-(5, 'José Martínez', 3, 50000.00),
-(6, 'Laura Sánchez', 3, 48000.00),
-(7, 'Pedro Ramírez', 1, 62000.00),
-(8, 'Sofía García', 2, 72000.00);
-
-SELECT * FROM Departamentos;
-SELECT * FROM Empleados;
-
-SELECT * FROM Empleados AS E
-JOIN Departamentos AS D ON E.DepartamentoID = D.ID;
-
--- pregunta 2
-SELECT Nombre, Salario FROM Empleados
-WHERE Salario > (SELECT MAX(E.Salario) FROM Empleados AS E 
-					JOIN Departamentos AS D ON E.DepartamentoID = D.ID 
-					WHERE D.Nombre = 'Ventas');
-
-SELECT E.Nombre FROM Empleados E
-WHERE E.Salario > ALL (SELECT Salario FROM Empleados WHERE DepartamentoID = (SELECT ID FROM Departamentos WHERE Nombre = 'Ventas'));
-
--- pregunta 3
-SELECT D.Nombre, (MAX(E.Salario) - MIN(E.Salario)) AS Diferencia
-FROM Departamentos AS D
-JOIN Empleados AS E
-ON E.DepartamentoID = D.ID 
-GROUP BY D.Nombre;
-
--- pregunta 4
-ALTER TABLE Departamentos
-ADD CONSTRAINT UQ_departamento UNIQUE (Nombre);
-
--- pregunta 5
-SELECT D.Nombre
-FROM Departamentos AS D
-JOIN Empleados AS E
-ON D.ID = E.DepartamentoID
-WHERE E.Salario > (SELECT Salario FROM )
